5a7e8f725b81e15c30feab2688fa523afd23b5d4
Joseph Nguyen <joseph6817@gmail.com>
2024-03-25 18:05:22 -0700

    first commit

---
+++ gamelet.js
@@ -0,0 +1,21 @@
+//test
+
+const ball = document.getElementById("ball");
+document.addEventListener("keydown", handleKeyPress);
+let position = 0;
+
+function handleKeyPress(e) {
+  if (e.code === "ArrowUp") {
+    position = position - 100;
+  }
+  if (e.code === "ArrowDown") {
+    position = position + 100;
+  }
+  if (position < 0) {
+    position = 0;
+  }
+  refresh();
+}
+function refresh() {
+  ball.style.top = position + "px";
+}
